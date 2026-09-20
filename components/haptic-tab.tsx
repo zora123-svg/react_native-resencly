0817a5d98478a3c08cf7a9eca6c8de2af1d7de00
Barisua Ntor-ue <donubari00@gmail.com>
2026-09-19 20:22:04 -0500

    update: resolved import  and compatablity issues

--- components/haptic-tab.tsx
+++ components/haptic-tab.tsx
@@ -1,8 +1,8 @@
-import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
-import { PlatformPressable } from '@react-navigation/elements';
 import * as Haptics from 'expo-haptics';
+import { PlatformPressable } from 'expo-router/react-navigation';
+import type { ComponentProps } from 'react';
 
-export function HapticTab(props: BottomTabBarButtonProps) {
+export function HapticTab(props: ComponentProps<typeof PlatformPressable>) {
   return (
     <PlatformPressable
       {...props}
