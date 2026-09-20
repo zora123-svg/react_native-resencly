022eeb29a9c61381262ff983c9eaa28b0011e6d2
Barisua Ntor-ue <donubari00@gmail.com>
2026-09-20 11:49:15 -0500

    project setup complete

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import "@/global.css"
+import { Text, View } from "react-native";
+ 
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
+        Welcome to Nativewind!!!
+      </Text>
+    </View>
+  );
+}
